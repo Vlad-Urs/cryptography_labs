a48dd1480693968c2db498e3e87a7cadfea57334
Vlad-Urs <113233447+Vlad-Urs@users.noreply.github.com>
2023-10-16 23:09:15 +0300

    optimized the program

--- cryptography-3.py
+++ cryptography-3.py
@@ -3,9 +3,6 @@ def print_menu():
     print('decrypt: 2')
     print('close: 3')
 
-def preprocess_text(text):
-    text = text.replace(" ", "").upper()
-    return text
 
 def validate_key(key):
     if len(key) < 7 or not key.isalpha():
@@ -43,11 +40,13 @@ def find_position(matrix, char):
             if matrix[i][j] == char:
                 return i, j
 
-def playfair_encrypt(plaintext, matrix):
-    ciphertext = ""
-    plaintext = preprocess_text(plaintext)
+def playfair(plaintext, matrix, operation):
+    if len(plaintext) % 2 != 0:
+        plaintext += 'V'
+        
+    text = ""
 
-    for i in range(0, len(plaintext), 2):
+    for i in range(0, len(plaintext) - 1, 2):
         char1 = plaintext[i]
         char2 = plaintext[i + 1]
 
@@ -55,41 +54,19 @@ def playfair_encrypt(plaintext, matrix):
         row2, col2 = find_position(matrix, char2)
 
         if row1 == row2:
-            col1 = (col1 + 1) % 5
-            col2 = (col2 + 1) % 5
+            col1 = (col1 + operation) % 5
+            col2 = (col2 + operation) % 5
         elif col1 == col2:
-            row1 = (row1 + 1) % 5
-            row2 = (row2 + 1) % 5
+            row1 = (row1 + operation) % 5
+            row2 = (row2 + operation) % 5
         else:
             col1, col2 = col2, col1
 
-        ciphertext += matrix[row1][col1] + matrix[row2][col2]
+        text += matrix[row1][col1] + matrix[row2][col2]
 
-    return ciphertext
+    return text
 
-def playfair_decrypt(ciphertext, matrix):
-    plaintext = ""
-    ciphertext = preprocess_text(ciphertext)
 
-    for i in range(0, len(ciphertext), 2):
-        char1 = ciphertext[i]
-        char2 = ciphertext[i + 1]
-
-        row1, col1 = find_position(matrix, char1)
-        row2, col2 = find_position(matrix, char2)
-
-        if row1 == row2:
-            col1 = (col1 - 1) % 5
-            col2 = (col2 - 1) % 5
-        elif col1 == col2:
-            row1 = (row1 - 1) % 5
-            row2 = (row2 - 1) % 5
-        else:
-            col1, col2 = col2, col1
-
-        plaintext += matrix[row1][col1] + matrix[row2][col2]
-
-    return plaintext
 
 if __name__ == "__main__":
     while True:
@@ -98,38 +75,38 @@ if __name__ == "__main__":
 
         if choice == '1':
             key = input('Enter the key (at least 7 letters, only letters): ')
-            key = key.replace(' ', '')
-            key = key.replace('j', 'i')
+            key = key.replace(' ', '').upper()
+            key = key.replace('J', 'I')
             if not validate_key(key):
                 print('Invalid key. Please follow the key requirements.')
                 continue
             matrix = build_playfair_matrix(key)
             plaintext = input('Enter the plaintext (letters only): ')
-            plaintext = plaintext.replace('j', 'i')
-            plaintext = plaintext.replace(' ', '')
+            plaintext = plaintext.replace(' ', '').upper()
+            plaintext = plaintext.replace('J', 'I')
             if not plaintext.isalpha():
                 print('enter a valid text')
                 continue
-            ciphertext = playfair_encrypt(plaintext, matrix)
+            ciphertext = playfair(plaintext, matrix, 1)
             print('Encrypted message:', ciphertext)
 
 
 
         elif choice == '2':
             key = input('Enter the key (at least 7 letters, only letters): ')
-            key = key.replace(' ', '')
-            key = key.replace('j', 'i')
+            key = key.replace(' ', '').upper()
+            key = key.replace('J', 'I')
             if not validate_key(key):
                 print('Invalid key. Please follow the key requirements.')
                 continue
             matrix = build_playfair_matrix(key)
             ciphertext = input('Enter the ciphertext (letters only): ')
-            ciphertext = ciphertext.replace('j', 'i')
-            ciphertext = ciphertext.replace(' ', '')
+            ciphertext = ciphertext.replace(' ', '').upper()
+            ciphertext = ciphertext.replace('J', 'I')
             if not ciphertext.isalpha():
                 print('enter a valid text')
                 continue
-            plaintext = playfair_decrypt(ciphertext, matrix)
+            plaintext = playfair(ciphertext, matrix, -1)
             print('Decrypted message:', plaintext)
         elif choice == '3':
             break
